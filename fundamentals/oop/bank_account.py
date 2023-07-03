aa99c16917da734a0149ad74e1922d2e5d16081f
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2023-07-03 15:39:26 -0700

    Users With Bank Accounts

--- fundamentals/oop/bank_account.py
+++ fundamentals/oop/bank_account.py
@@ -1,7 +1,8 @@
 class BankAccount:
   all_accounts = []
 
-  def __init__(self, balance = 0, int_rate = 0.01):
+  def __init__(self, name, balance = 0, int_rate = 0.01):
+    self.account_name = name
     self.balance = balance
     self.rate = int_rate
     BankAccount.all_accounts.append(self)
@@ -10,12 +11,15 @@ class BankAccount:
   def print_bankAccount_info(cls):
     print("-----------------")
     for account in cls.all_accounts:
+      print(f"Account name: ${account.account_name}")
       print(f"Balance: ${account.balance}")
-      print(f"Rate: {account.rate}%")
+      print(f"Rate: {account.rate*100}%")
+    return cls
 
   def deposit(self, amount):
     self.balance += amount
     return self
+
   def withdraw(self, amount):
     if(self.balance - amount >= 0):
       self.balance -= amount
@@ -24,19 +28,21 @@ class BankAccount:
       print( "Insufficient funds: Charging a $5 fee")
       self.balance - 5
       return self
+
   def display_account_info(self):
     print(f"Balance: ${self.balance}")
     return self
+
   def yield_interest(self):
     if (self.balance > 0):
-      self.balance = self.balance + self.balance * (self.rate/100)
+      self.balance = self.balance + self.balance * self.rate
       return self
 
-account1 = BankAccount(1000, 5)
-account2 = BankAccount(2000, 10)
+# account1 = BankAccount(1000, 0.05)
+# account2 = BankAccount(2000, 0.1)
 
-account1.deposit(100).deposit(200).deposit(300).withdraw(400).yield_interest().display_account_info()
-account2.deposit(150).deposit(320).withdraw(100).withdraw(200).withdraw(160).withdraw(430).yield_interest().display_account_info()
+# account1.deposit(100).deposit(200).deposit(300).withdraw(400).yield_interest().display_account_info()
+# account2.deposit(150).deposit(320).withdraw(100).withdraw(200).withdraw(160).withdraw(430).yield_interest().display_account_info()
 
 
-BankAccount.print_bankAccount_info()
+# BankAccount.print_bankAccount_info()
---
+++ fundamentals/oop/users-with-bank-accounts.py
@@ -0,0 +1,52 @@
+from bank_account import BankAccount
+
+
+class User:
+  def __init__(self, name, email):
+      self.name = name
+      self.email = email
+      self.accounts = []
+
+  def create_account(self, account_name):
+    self.accounts.append(BankAccount( account_name, balance=0, int_rate=0.02))
+    return self
+
+  def make_deposit(self, amount, account_name):
+    for account in self.accounts:
+      if (account.account_name == account_name):
+        account.deposit(amount)
+    return self
+
+  def make_withdrawal(self, amount, account_name):
+    for account in self.accounts:
+      if (account.account_name == account_name):
+        account.withdraw(amount)
+    return self
+
+  def display_user_accounts(self):
+    print("====================")
+    print(self.name)
+    for account in self.accounts:
+      print("-----------------")
+      print(account.account_name)
+      print(account.balance)
+      print(account.rate)
+    return self
+
+  def transfer_money(self, amount,  other_user):
+    self.accounts[0].balance -= amount
+    other_user.accounts[0].balance += amount
+
+user1 = User("Bob", "bob@gmail.com")
+user1.create_account("Savings").create_account("Checking").make_deposit(200, "Savings").make_deposit(300, "Checking").make_withdrawal(150, "Checking")
+
+user2 = User("John", "john@gmail.com")
+user2.create_account("Savings").create_account("Checking").make_deposit(100, "Savings").make_deposit(500, "Checking").make_withdrawal(200, "Checking")
+
+user1.transfer_money(30, user2)
+
+
+
+user1.display_user_accounts()
+user2.display_user_accounts()
+
