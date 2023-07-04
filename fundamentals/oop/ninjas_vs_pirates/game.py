0b9c1bce008255bf998b395ab92e908dde8ea838
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2023-07-03 23:28:46 -0700

    Ninjas vs Pirates

---
+++ fundamentals/oop/ninjas_vs_pirates/game.py
@@ -0,0 +1,10 @@
+from classes.ninja import Ninja
+from classes.pirate import Pirate
+
+michelangelo = Ninja("Michelanglo")
+
+jack_sparrow = Pirate("Jack Sparrow")
+
+michelangelo.attack(jack_sparrow)
+jack_sparrow.show_stats()
+michelangelo.show_stats()
