8f5fc65af96f2953bca5f494d3a44c07b3b71a30
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2023-07-06 16:30:45 -0700

    Checkerboard

--- flask/checkerboard/server.py
+++ flask/checkerboard/server.py
@@ -3,23 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-  return "Hello World"
-
-
-
-@app.route('/play/')
-def welcome_play():
-  return render_template("index.html", times=3, color="blue")
-
-@app.route('/play/<int:times>/')
-def welcome_play_number(times):
-  return render_template("index.html", times=times, color="blue")
-
-@app.route('/play/<int:times>/<string:color>/')
-def welcome(times, color):
-  return render_template("index.html", times=times, color=color)
-
+@app.route('/<int:y>/')
+@app.route('/<int:x>/<int:y>/')
+@app.route('/<int:x>/<int:y>/<string:light>/')
+@app.route('/<int:x>/<int:y>/<string:light>/<string:dark>/')
+def colors(x=8, y=8, light="blue", dark="black"):
+  return render_template("index.html", x=x, y=y, light=light, dark=dark)
 
 if __name__=="__main__":
   app.run(debug=True)
